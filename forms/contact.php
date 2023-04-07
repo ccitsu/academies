32aed2534e767ee5c196aad5baa3c641861dd1b6
AATCe-ShaqraUniversity <ccitlms@gmail.com>
2023-04-07 16:15:05 +0300

    Updates!

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,10 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'ccithelpit@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Unable to load the "Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
@@ -26,9 +18,9 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'ccithelpit',
+    'password' => 'password',
     'port' => '587'
   );
   */
